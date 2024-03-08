dc2d390fd1e5ccd66d9367f5b2cbe9719a5031b5
Mohamed Zahi <mohamedzahi33@gmail.com>
2024-03-08 16:59:41 +0400

    first commit

---
+++ extract_proxies.py
@@ -0,0 +1,19 @@
+import csv
+
+def read_csv_and_create_2d_list(csv_file_path):
+    result_2d_list = []
+
+    with open(csv_file_path, 'r') as file:
+        csv_reader = csv.reader(file)
+
+        for row in csv_reader:
+            proxy = row[0] + ':' + row[7]
+ 
+            result_2d_list.append(proxy)
+
+    with open("output.txt", "w") as file:
+        for element in result_2d_list:
+            file.write(str(element) + "\n")
+
+# I will take all my money from people
+read_csv_and_create_2d_list('Free_Proxy_List.csv')
